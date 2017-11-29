15dc780b5f985e5c22f20cbe059a5e8d6769bbe6
Girard Morgan <mgirard@etud.insa-toulouse.fr>
2017-11-29 09:33:17 +0100

    Refactoring de ShippingCostsCalculator pour inclure la partie multiplicative et constante d'un frais de port dans l'enumeration Destination

--- src/com/Boysband/ShippingCostsCalculator.java
+++ src/com/Boysband/ShippingCostsCalculator.java
@@ -2,16 +2,27 @@ package com.Boysband;
 
 public class ShippingCostsCalculator {
     public enum Destination {
-        France,
-        Monaco
+        France(0.0, 0.0),
+        Monaco(8.7, 0.0);
+
+        private double multiplyBy;
+        private double add;
+
+        Destination(double multiplyBy, double add) {
+            this.multiplyBy = multiplyBy;
+            this.add = add;
+        }
+
+        public double getMultiplyBy() {
+            return multiplyBy;
+        }
+
+        public double getAdd() {
+            return add;
+        }
     }
     public static double calculateShippingCost(Package pack, Destination destination){
-        if(destination==Destination.France){
-            return pack.calculateLocalShippingCost();
-        } else if (destination==Destination.Monaco){
-            return pack.calculateLocalShippingCost()*(1+(8.7/100));
-        } else {
-            return 0;
-        }
+        double baseValue = pack.calculateLocalShippingCost();
+        return baseValue*(1+destination.getMultiplyBy()/100)+destination.getAdd();
     }
 }
